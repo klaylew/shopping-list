91356cfba97f3ad5ad6eafaaa1cfc8fcc8088f54
Klayton Lewis <klayton.lewis@gmail.com>
2020-09-22 18:17:37 +0000

    added move checked item to end of list

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ function addItem() {
   
   $("#js-shopping-list-form").submit(event => {
    event.preventDefault(event);
+   const itemName = $(event.currentTarget).find('#shopping-list-entry');
    
   $(".shopping-list").prepend(`
-    <li>
-        <span class="shopping-item">test</span>
+      <li>
+        <span class="shopping-item">${itemName.val()}</span>
         <div class="shopping-item-controls">
           <button class="shopping-item-toggle">
             <span class="button-label">check</span>
@@ -17,6 +18,7 @@ function addItem() {
         </div>
       </li>
       `) 
+      itemName.val("");
 
   })
  }
@@ -24,17 +26,26 @@ function addItem() {
 
 //check and uncheck items on the list by clicking the "Check" button
 function checkOffItem() {
-  $(".shopping-item-toggle").click(function(event) {
-    $(event.currentTarget).closest('.shopping-item').toggleClass('.shopping-item__checked');
-  })  
+  /*
+    code that worked for functionaility on existing items:
+    $(".shopping-item-toggle").on('click', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  })
+  */
+  //function that works on dynamically coded objects by listening to the parent element "ul"
+  $('ul').on('click', 'li .shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked').parent().appendTo($('ul'));
+  })
 }
-
-
+  
 //permanently remove items from the list
 function deleteItem() {
-
+  $('ul').on('click', 'li .shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  })
 }
 
 
 $(addItem);
 $(checkOffItem);
+$(deleteItem);
